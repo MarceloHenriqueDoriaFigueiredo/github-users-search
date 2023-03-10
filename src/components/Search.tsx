f6e05df5256bb8ea50f962399c7325e6b3586be0
Marcelo Figueiredo <marchenrique99@gmail.com>
2023-03-10 08:11:07 -0300

    aplicando estilizações

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,17 +2,18 @@ type SearchProps = {
     loaderUser: (userName: string) => Promise<void>
 }
 
+import styles from './Search.module.css'
 import { useState } from 'react'
 import { BsSearch } from 'react-icons/bs'
 export function Search({loaderUser}: SearchProps) {
     
     const [userName, setUserName] = useState('')
     return(
-        <div>
+        <div className={styles.search}>
             <h2>Busque por usuario:</h2>
             <p>Conheça os melhores repositorios</p>
 
-            <div>
+            <div className={styles.search_container}>
                 <input type="text" placeholder="Digite o nome do usuario" 
                 onChange={(e) => setUserName(e.target.value)} />
                 <button onClick={() => loaderUser(userName)}>
