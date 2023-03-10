2d0c6c81faa6e2c0a91814e97ebb68042bf7dd81
Marcelo Figueiredo <marchenrique99@gmail.com>
2023-03-10 08:19:30 -0300

    busca por botao enter

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,19 +3,29 @@ type SearchProps = {
 }
 
 import styles from './Search.module.css'
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
 import { BsSearch } from 'react-icons/bs'
 export function Search({loaderUser}: SearchProps) {
     
     const [userName, setUserName] = useState('')
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Enter") {
+            loaderUser(userName)
+        }
+    }
     return(
         <div className={styles.search}>
             <h2>Busque por usuario:</h2>
             <p>Conheça os melhores repositorios</p>
 
             <div className={styles.search_container}>
-                <input type="text" placeholder="Digite o nome do usuario" 
-                onChange={(e) => setUserName(e.target.value)} />
+                <input type="text" 
+                placeholder="Digite o nome do usuario" 
+                onChange={(e) => setUserName(e.target.value)} 
+                onKeyDown={handleKeyDown}
+                />
+                
                 <button onClick={() => loaderUser(userName)}>
                     <BsSearch />
                 </button>
